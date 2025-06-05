eee38deaedf0d744eb4e1080d931e87e6655b1ec
opticayaan <ayaan.gill@me.com>
2025-06-04 21:20:10 -0400

    Fix why Layer text

--- app/_components/sections/WhyLayer.tsx
+++ app/_components/sections/WhyLayer.tsx
@@ -6,10 +6,10 @@ const WhyLayer = () => {
       <div className="w-full max-w-[--content-width] flex flex-col gap-2 text-center py-16 z-10 relative p-8 md:px-16 border-x-[1px] border-x-gray-900">
         <h2 className="section-header">WHY LAYER?</h2>
         <p className="text-2xl text-left">
-          We’ve been building “MCP Servers” since a year before MCP even
-          existed. We’ve built servers for XYZ, have seen what does and doesn’t
-          work, and often speak about the MCP ecosystem and best practices for
-          building servers.
+          Our team was building Al plugins a year before MCP existed. Since
+          then, we've built over 100 MCP servers and have seen what works and
+          what doesn't. We often give talks about developments in the MCP
+          ecosystem and best practices for building servers.
         </p>
       </div>
     </div>
